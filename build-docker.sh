b50e4f2b8ef0e377385999a89e97cdb72f302777
AbdAlrahman AlaaEldeen <alaaeldeen.abdalrahman@zmp.co.jp>
2020-08-02 11:27:56 +0900

    Initial commit Changes to be committed: 	new file:   Dockerfile  >> Dockerfile to use Ros melodic Image and install the required Env 	new file:   build-docker.sh  >> Build script mainly to get the right Nvidia support 	new file:   run >> Run script to be used as an example to run the container in the correct way, this should be excuted inside the root directory of the repository

---
+++ build-docker.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/hatem-darweesh/assuremappingtools.git
+
+version="$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev)"
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/"$version"/NVIDIA-Linux-x86_64-"$version".run
+mv NVIDIA-Linux-x86_64-"$version".run NVIDIA-DRIVER.run
+
+docker build -t assurmaptool .
