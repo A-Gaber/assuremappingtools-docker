cebb970f55ef23ce630e352f3e3d8017c95cb94e
AbdAlrahman AlaaEldeen <alaaeldeen.abdalrahman@zmp.co.jp>
2020-08-02 11:41:41 +0900

    Checkout ubuntu18.04_build branch from the tool repository

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/hatem-darweesh/assuremappingtools.git
+git clone https://github.com/hatem-darweesh/assuremappingtools.git --branch ubuntu18.04_build
 
 version="$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev)"
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/"$version"/NVIDIA-Linux-x86_64-"$version".run
